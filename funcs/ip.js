dc78163fd376c50d7760223f0d2dd1cfbe7f9306
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-11-30 19:49:16 -0300

    feat(ip): ip authorization

--- funcs/ip.js
+++ funcs/ip.js
@@ -2,7 +2,7 @@ async function queryIp(){
   try{
     const ip = await fetch("https://api64.ipify.org/?format=json")
     const json = ip.json()
-    return json.ip
+    return json
   }catch(error){
     console.error(error)
     return null
