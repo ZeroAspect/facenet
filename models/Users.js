8cd0bf0f8661504b3605d47fe724342137ca66ad
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-11-30 17:11:06 -0300

    feat(models/User): creating new model User

---
+++ models/Users.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize/sequelize.js");
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      min: 8,
+      max: 700
+    },
+  },
+  biografia:{
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  ip: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  }
+});
+
+module.exports = User
