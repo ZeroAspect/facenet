302120e05ef990ae9ccce66529c0c37ea498ce72
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-11-30 16:34:01 -0300

    feat(index): server listen

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = require("./config/config.js")
+
+require("./app.js")
+
+app.listen(3000, (err)=>{
+  if(err) throw err
+  console.log("Server is running on port 3000")
+})
